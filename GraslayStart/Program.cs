9587afb3f4cafa9f3f85c198341937f0a228caea
Ontake44 <ogu-take@nifty.com>
2022-09-01 22:30:58 +0900

    起動プログラム追加

---
+++ GraslayStart/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GraslayStart
+{
+	static class Program
+	{
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+			string folderPath = System.IO.Path.GetDirectoryName(exePath);
+			System.IO.Directory.SetCurrentDirectory(folderPath);
+			
+			System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+			startInfo.FileName = "pyxel.exe";
+			startInfo.Arguments = "play graslay.pyxapp";
+			startInfo.CreateNoWindow = true;
+			startInfo.UseShellExecute = false;
+			System.Diagnostics.Process.Start(startInfo);
+		}
+	}
+}
